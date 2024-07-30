3bbf00046c55bbafdc83af39d053c24fb84f4a33
SCFC-CN <130237164+SCFC-CN@users.noreply.github.com>
2024-07-31 00:14:22 +0800

    Update music.js

--- js/music.js
+++ js/music.js
@@ -11,7 +11,7 @@ GitHub：https://github.com/imsyy/home
 */
 var server = "netease"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
 var type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
-var id = "10113950624"; //封面 ID / 单曲 ID / 歌单 ID
+var id = "9283925351"; //封面 ID / 单曲 ID / 歌单 ID
 
 $.ajax({
     url: "https://api-meting.imsyy.top/api?server=" + server + "&type=" + type + "&id=" + id,
